9504670ce67e49fe607ea64f2c793d16349f40d8
PrakashNair <prakashn@adobe.com>
2025-03-21 20:52:41 -0400

    Custom 1.0

---
+++ myaction/__main__.py
@@ -0,0 +1,14 @@
+import argparse
+
+parser = argparse.ArgumentParser(__name__)
+
+parser_github = parser.add_argument_group("github")
+parser_github.add_argument("-gt", "--github-token")
+parser_github.add_argument("-gr", "--github-repository")
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    print(args)
+
+    print(f " >>  {arguments.github_repository}")
+
