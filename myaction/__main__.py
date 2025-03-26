bd2383f6e7e1628dec8019220fb20f085ac225e9
PrakashNair <prakashn@adobe.com>
2025-03-26 17:51:15 -0400

    Support secret

--- myaction/__main__.py
+++ myaction/__main__.py
@@ -5,10 +5,11 @@ parser = argparse.ArgumentParser(__name__)
 parser_github = parser.add_argument_group("github")
 parser_github.add_argument("-gt", "--github-token")
 parser_github.add_argument("-gr", "--github-repository")
+parser_github.add_argument("-scr", "--input-secret-token")
 
 if __name__ == "__main__":
     args = parser.parse_args()
     print(args)
 
-    print(f" >>  {args.github_repository}")
+    print(f" >>  {args.input_secret_token}")
 
